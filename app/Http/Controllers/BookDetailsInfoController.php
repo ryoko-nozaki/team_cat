c6d7d9cb8850887db9d027bb0a9e8cea4979c9e7
amepop1280 <amepop.1280@gmail.com>
2018-11-19 04:59:15 +0900

    書籍詳細ページ更新

--- app/Http/Controllers/BookDetailsInfoController.php
+++ app/Http/Controllers/BookDetailsInfoController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\support\Facades\Auth;
 
 class BookDetailsInfoController extends Controller
 {
-    public function index($booksId) {
-        return view('bookDetailsInfo.index');
+    public function index($bookId=null) {
+        $user = Auth::user();
+        $param = ['user' => $user];
+        return view('bookDetailsInfo.index', $param);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/book/{bookId?}', 'BookDetailsInfoController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
